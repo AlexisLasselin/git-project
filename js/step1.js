a92e5367db21efce4fb059dcea33fc83b02d45bc
AlexisLasselin <114481578+AlexisLasselin@users.noreply.github.com>
2022-11-09 11:41:03 +0100

    discover of js

---
+++ js/step1.js
@@ -0,0 +1,32 @@
+console.log("Hello from JS");
+
+// const red = "#f00";
+// console.log(red);
+
+// let postTitle = "JavaScript/ECMAScript";
+// postTitle = "HTML/CSS";
+// console.log(postTitle);
+
+// let a = 5;
+// console.log(a);
+// a = 'FIVE';
+// console.log(a);
+
+// if (a == 5) {
+//     console.log(5 + 10);
+// } else {
+//     console.log("Fifteen");
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// for (let i = 2; i <= 20; i += 2) {
+//     console.log(i);
+// }
+
+// for (let i = 1; i <=10; i++) {
+//     console.log(2**i);
+// }
+
