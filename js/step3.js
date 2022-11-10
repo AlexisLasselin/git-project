523d27404492b7fc3a12ddfd94a6f3134f002439
AlexisLasselin <114481578+AlexisLasselin@users.noreply.github.com>
2022-11-10 11:46:25 +0100

    changes

--- js/step3.js
+++ js/step3.js
@@ -5,6 +5,19 @@ console.log(p2.id);
 
 const imgSwap = document.querySelector('#image-display img');
 const changeBtn = document.querySelector('#image-display button');
+
+let isFirst = true;
 changeBtn.addEventListener('click', () => {
+    if (isFirst) {
     imgSwap.src = '../ressources/rickroll-roll.gif';
+    isFirst = false;
+    } else {
+    imgSwap.src = '../ressources/code.jpg';
+    isFirst = true;
+    }
+});
+
+const showBtn = document.querySelector('#show-btn');
+showBtn.addEventListener('click', () => {
+    imgSwap.classList.toggle('hide');
 });
